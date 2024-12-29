d8c27ee140e42ea7c00fff32339d737060cfdd6e
om <om.sudhakarrecruit@gmail.com>
2024-12-29 21:53:20 +0530

    second

--- test_app.py
+++ test_app.py
@@ -1,2 +1,10 @@
 #this is for unit testing
 #pytest can recognise if the file name starts with test, so this file name test_
+from app import app
+
+def test_home():
+    response=app.test_client().get("/") #we get responce from homw
+    #checking responce using assert
+    assert response.status_code==200 #if successful
+    assert response.data=="successful"
+    
